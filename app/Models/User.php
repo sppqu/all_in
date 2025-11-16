99cd0c107f34bb5660cbd6a77a5e49db2257fdf2
Agus Munif, S.Kom <mysppqu@gmail.com>
2025-11-16 18:26:25 +0700

    Add school_id to User model fillable array
    - Add school_id to fillable array to allow mass assignment
    - Fixes SQL error: Field 'school_id' doesn't have a default value
    - school_id is now properly included in User::create() operations

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name', 'email', 'password', 'nomor_wa', 'role', 'is_bk', 'spmb_admin_access',
+        'name', 'email', 'password', 'nomor_wa', 'role', 'is_bk', 'spmb_admin_access', 'school_id',
     ];
 
     /**
